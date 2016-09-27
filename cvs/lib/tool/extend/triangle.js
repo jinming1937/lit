cf6a7513786f0b1de8546fea7176d5a5d84d0fe0
jinming <jinming1937@163.com>
2016-09-27 19:46:17 +0800

    udpate lit

--- cvs/lib/tool/extend/triangle.js
+++ cvs/lib/tool/extend/triangle.js
@@ -9,8 +9,16 @@ define([
 	 * @param {Number} b b
 	 * @param {Number} c c
 	 * @param {Number} angle
+	 * @param {Number} x 顶角x 坐标
+	 * @param {Number} y 顶角y 坐标
 	 */
 	function Triangle(config){
+		Action.call(this);
+		Watch.call(this);
+		Element.call(this);
+		config.x = config.x || 0;
+		config.y = config.y || 0;
+		this.config = config;
 		/* 判断是否是三角形：
 		 * 1 ： 任意两边之和大于第三边
 		 * 2 ： 知道两边长，和其夹角
@@ -21,10 +29,34 @@ define([
 		}else{
 			this.angleAb(config.a, config.b, config.angle);
 		}
+		
+		this.draw();
 	}
 	
 	Triangle.prototype.abc = function(a,b,c){
-		
+		var _this = this;
+		_this.positionXYArray = [];
+		/* 顶角坐标 */
+		_this.positionXYArray.push({
+			x:_this.config.x,
+			y:_this.config.y
+		});
+		/* 任意两边和大于第三边 */
+		if(a + b > c && a + c > b && b + c > a){
+			_this.positionXYArray.push({
+				x:a,
+				y:_this.config.y
+			});
+			/**
+			 *  x*x + y*y = c*c
+			 *  
+			 * 
+			 * 
+			 */
+			
+		}else{
+			throw("NOT A TRIANGLE!!!");
+		}
 	};
 	
 	Triangle.prototype.angleAb = function(a,b,angle){
@@ -35,9 +67,6 @@ define([
 			throw("angle is not avilible");
 		}
 	};
-	
-	
-	
 	
 	Triangle.prototype.checkTriangle = function(){
 		
