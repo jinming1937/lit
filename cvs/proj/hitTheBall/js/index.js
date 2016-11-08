1fe8a99dbd03550b65549e8109256f0d4d204ad2
jinming <jinming1937@163.com>
2016-11-09 02:41:11 +0800

    update lit

--- cvs/proj/hitTheBall/js/index.js
+++ cvs/proj/hitTheBall/js/index.js
@@ -3,15 +3,18 @@ define([
 	"../../../lib/tool/extend/triangle",
 	"../../../lib/tool/circle",
 	"../../../lib/server/ws",
-	"../../../outer/gctrl"
-],function(button,triangle,circle,ws,gctrl){
+	"../../../outer/gctrl",
+	"../../../lib/server/storage"
+],function(button,triangle,circle,ws,gctrl,storage){
 	main.on("show","index",function(){
 		var frame = (main.getCurrentFrame()).cvs;
 		var screenWidth = (main.getCurrentFrame()).cvs.width;
 		var screenHeight = (main.getCurrentFrame()).cvs.height;
 		new gctrl(frame);
 		console.log("begin load index");
-
+		var thisUser = storage.cookieCtrl.getData("BCookieID");
+		var thisStdn = storage.cookieCtrl.getData("stdn");
+		var sData = JSON.parse($data);
 		/*
 			todo: 1、 适配机型，不同大小的屏幕如何无缝对战？ 
 				  2、 各自控制各自的按钮，红控红，蓝控蓝
@@ -26,7 +29,12 @@ define([
 		data = data ||{};
 
 		ws.onopen = function(){
-		  ws.send(JSON.stringify({x:0}));
+			var dt = {};
+			dt.x = screenWidth/2;
+			dt.userID = thisUser;
+			dt.roomNum = 0;
+			dt.stdn = thisStdn;
+			ws.send(JSON.stringify(dt));
 		};
 
 		var red = new circle({
@@ -34,8 +42,8 @@ define([
 			y:screenHeight/6 - 15,
 			radius:15,
 			strong:true,
-			backgroundColor:"#F00",
-			color:"#F00",
+			backgroundColor:sData.color,
+			color:sData.color,
 			// ontouchmove:function(e){
 			// 	red.y = screenHeight/6 - 15;
 			// 	if(e.changedTouches[0].clientX + this.radius > screenWidth){
@@ -62,8 +70,8 @@ define([
 			y: screenHeight/6 * 5 - 15,
 			radius:15,
 			strong:true,
-			backgroundColor:"#00F",
-			color:"#00F",
+			backgroundColor:sData.bcolor,
+			color:sData.bcolor,
 			ontouchmove:function(e){
 				blue.y = screenHeight/6*5 - 15;
 				if(e.changedTouches[0].clientX + this.radius > screenWidth){
@@ -80,7 +88,9 @@ define([
 		blue.addWatching("touchmove",function(e){
 			var dt = {};
 			dt.x = parseInt(blue.x);
-
+			dt.userID = thisUser;
+			dt.roomNum = 0;
+			dt.stdn = thisStdn;
 			ws.send(JSON.stringify(dt));
 			console.log("send message:" + dt.x); 
 			//cancelAnimationFrame(timeTip);
