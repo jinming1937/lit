7d320bcb92b5a4a2ff76e86ee15442bfbe29683d
jinming <jinming1937@163.com>
2016-11-08 13:05:01 +0800

    update lit

--- cvs/proj/hitTheBall/mainForBall.js
+++ cvs/proj/hitTheBall/mainForBall.js
@@ -10,11 +10,20 @@ require.config({
 });
 
 define(["../../lib/main","./js/index"],function(main){
-	main.router.addRouter({
-		index:0,
-		cvsName : "index",
-		urlReg:/index/
-	});
+	if(location.port === "8089"){
+		main.router.addRouter({
+			index:0,
+			cvsName : "index",
+			urlReg:/index/
+		});
+	}else{
+		main.router.addRouter({
+			index:0,
+			cvsName : "index",
+			urlReg:/ball\/ball/
+		});
+	}
+	
 	main.init();
 	console.log("ending");
 });
