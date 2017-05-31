fe4d42452068519dd2d34eb72dca3f3539a970fd
jinming <jinming1937@163.com>
2017-05-31 11:15:21 +0800

    更新四叶草

--- proj/snake/js/nineBalls.js
+++ proj/snake/js/nineBalls.js
@@ -14,74 +14,6 @@ core.on("show", "nineballs", function(cvs) {
         endY: cvs.canvas.height
     });
 
-    var threeArray = [];
-
-    function getPoi(num) {
-        var positionArray = [];
-        var baseNum = 0;
-        for (var i = 1; i <= num; i++) {
-            baseNum += i;
-            switch (i % 4) {
-                case 0:
-                    positionArray.push({
-                        x: -baseNum,
-                        y: 0
-                    });
-                    break;
-                case 1:
-                    positionArray.push({
-                        x: 0,
-                        y: -baseNum
-                    });
-                    break;
-                case 2:
-                    positionArray.push({
-                        x: baseNum,
-                        y: 0
-                    });
-                    break;
-                case 3:
-                    positionArray.push({
-                        x: 0,
-                        y: baseNum
-                    });
-                    break;
-            }
-        }
-        return positionArray;
-    }
-
-    function fbnq(num) {
-        // console.log(num);
-        if (num === 1) {
-            return 0;
-        } else if (num === 2) {
-            return 1;
-        } else {
-            return fbnq(num - 1) + fbnq(num - 2);
-        }
-    }
-
-    function drawFour(m) {
-        var po = getPoi(m);
-        po.forEach(function(item, index) {
-            // new PointPackage.Point({
-            //     x: cvs.canvas.width / 2 + item.x,
-            //     y: cvs.canvas.height / 2 + item.y,
-            // });
-            // new FourPackage.Four({
-            //     x: cvs.canvas.width / 2 + item.x,
-            //     y: cvs.canvas.height / 2 + item.y,
-            //     radius: Math.max(Math.abs(item.x), Math.abs(item.y)),
-            //     rouNum: 3,
-            //     xRate: 0,
-            //     yRate: 0.3,
-            //     fillStyle: "rgba(6,179,60,0.8)"
-            // });
-        });
-    }
-    // drawFour(15);
-
     new FourPackage.Four({
         x: 150,
         y: 150,
@@ -102,41 +34,41 @@ core.on("show", "nineballs", function(cvs) {
         fillStyle: "rgba(6,179,60,0.8)"
     });
 
-    // var fourArray = new FourPackage.Four({
-    //     x: 150,
-    //     y: 150,
-    //     radius: 80,
-    //     xRate: 0,
-    //     yRate: 0.4,
-    //     fillStyle: "rgba(6,179,60,0.8)"
-    // });
-    // fourArray.addWatching("touchmove", function(e) {
-    //     this.x = e.changedTouches[0].clientX; //- moving.width / 2;
-    //     this.y = e.changedTouches[0].clientY; //- moving.width / 2;
-    // });
+    var fourArray = new FourPackage.Four({
+        x: 150,
+        y: 150,
+        radius: 80,
+        xRate: 0,
+        yRate: 0.4,
+        fillStyle: "rgba(6,179,60,0.8)"
+    });
+    fourArray.addWatching("touchmove", function(e) {
+        this.x = e.changedTouches[0].clientX; //- moving.width / 2;
+        this.y = e.changedTouches[0].clientY; //- moving.width / 2;
+    });
 
-    // var goHome = new RoundRectPackage.RoundedRect({
-    //     className: "button goHome",
-    //     fontColor: '#101010',
-    //     cornerX: cvs.canvas.width - 20 - 80,
-    //     cornerY: cvs.canvas.height - 20 - 30,
-    //     width: 90,
-    //     height: 30,
-    //     cornerRadius: 8,
-    //     fillText: "HomePage"
-    // });
-    // goHome.ontouchend = function(e) {
-    //     stopGame();
-    //     isStop = true;
-    //     core.open({
-    //         href: urlPath + "snake/snindex.html"
-    //     });
-    // };
+    var goHome = new RoundRectPackage.RoundedRect({
+        className: "button goHome",
+        fontColor: '#101010',
+        cornerX: cvs.canvas.width - 20 - 80,
+        cornerY: cvs.canvas.height - 20 - 30,
+        width: 90,
+        height: 30,
+        cornerRadius: 8,
+        fillText: "HomePage"
+    });
+    goHome.ontouchend = function(e) {
+        stopGame();
+        isStop = true;
+        core.open({
+            href: urlPath + "snake/snindex.html"
+        });
+    };
     var rotateRate = 0;
 
     function updateBall(fpx) {
         rotateRate = (rotateRate + 36 * fpx > 360) ? (360 - rotateRate + 36 * fpx) : rotateRate + 36 * fpx;
-        // fourArray.rotateRate = rotateRate * Math.PI * 2 / 360;
+        fourArray.rotateRate = rotateRate * Math.PI * 2 / 360;
         // ball.y = (ball.y + 20 * fpx) > core.frame.height + 100 ? -100 : (ball.y + 20 * fpx);
     }
 
@@ -165,7 +97,7 @@ core.on("show", "nineballs", function(cvs) {
                 lastTime = startTime;
                 return;
             }
-            // updateBall(fpx);
+            updateBall(fpx);
             core.frame.reRender();
             var xfps = fps_text.getFps(+new Date()).toFixed();
             fpsWord.word = flagNum ? xfps : fpsWord.word;
